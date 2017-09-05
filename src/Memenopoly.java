6004e550c28de606f6c80c1022d4401f497744b3
varnit05 <league@WTS20.attlocal.net>
2017-09-04 19:58:14 -0700

    jeff
    Created the classes

---
+++ src/Memenopoly.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class Memenopoly {
+	JFrame frame;
+	final static int frameHeight=950;
+	final static int frameWidth=1800;
+	GamePanel gamePanel;
+	public Memenopoly() {
+		frame = new JFrame();
+		gamePanel = new GamePanel();
+		frame.addKeyListener(gamePanel);
+		setup();
+	}
+	public static void main(String[] args) {
+		Memenopoly m = new Memenopoly();
+	}
+	void setup() {
+		frame.add(gamePanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(frameWidth, frameHeight);
+		gamePanel.startGame();
+	}
+}
